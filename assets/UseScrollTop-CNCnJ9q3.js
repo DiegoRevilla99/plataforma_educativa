993ef3015a2c9e56caf610da43fb41d328070724
Jose Antonio Diego Revilla <75765142+DiegoRevilla99@users.noreply.github.com>
2024-09-18 10:30:00 -0600

    Delete assets directory

--- assets/UseScrollTop-CNCnJ9q3.js
+++
@@ -1,1 +0,0 @@
-import{b as s}from"./index-FO23yod1.js";const r=(o=!0)=>{window.scrollTo({top:0,behavior:o?"smooth":"auto"})},l=()=>{const o=s.useCallback((t=!0)=>{r(t)},[]);s.useEffect(()=>{o()},[])};export{l as u};
