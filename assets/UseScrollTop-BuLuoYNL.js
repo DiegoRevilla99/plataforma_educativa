10aa2ed865fce0eabf2083f78148299667a55857
Jose Antonio Diego Revilla <75765142+DiegoRevilla99@users.noreply.github.com>
2024-09-27 16:54:10 -0600

    Add files via upload

---
+++ assets/UseScrollTop-BuLuoYNL.js
@@ -0,0 +1,1 @@
+import{b as s}from"./index-DjusE7W-.js";const r=(o=!0)=>{window.scrollTo({top:0,behavior:o?"smooth":"auto"})},l=()=>{const o=s.useCallback((t=!0)=>{r(t)},[]);s.useEffect(()=>{o()},[])};export{l as u};
