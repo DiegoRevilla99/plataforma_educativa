71481af719dc42b992bfdf743aaf0b629fb7afa9
Jose Antonio Diego Revilla <75765142+DiegoRevilla99@users.noreply.github.com>
2024-10-02 13:48:16 -0600

    Delete assets directory

--- assets/HeaderTemplate-Beu40it2.js
+++
@@ -1,1 +0,0 @@
-import{j as e,B as o}from"./index-BI8o-9Ro.js";const t=()=>e.jsx(o,{height:"10dvh",overflow:"hidden",bgcolor:"#f2f2f2"});export{t as H};
--- assets/Home-OejB-JLI.js
+++
@@ -1,1 +0,0 @@
-import{j as e,A as o,B as t}from"./index-BI8o-9Ro.js";import{H as s}from"./HeaderTemplate-Beu40it2.js";const i=()=>e.jsxs(o,{children:[e.jsx(s,{}),e.jsx(t,{width:"100%",height:"90dvh",display:"flex",bgcolor:"#f2f2f2",flexDirection:"column"})]});export{i as default};
